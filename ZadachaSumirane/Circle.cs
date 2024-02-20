5410c6ab6f9a8cc720244fa6af48a123f55f6924
pc1-17 <pc1-17@DESKTOP-UU3I7S5>
2024-02-20 09:02:19 +0200

    Add project files.

---
+++ ZadachaSumirane/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ZadachaSumirane
+{
+    internal class Circle : Shape
+    {
+
+
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+              Radius = radius;
+        }
+        public override double calculateArea()
+        {
+           return Math.PI * Radius * Radius;
+        }
+
+        public override double calculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+        public override string Draw()
+        {
+            "circle";
+        }
+    }
+}
---
+++ ZadachaSumirane/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadachaSumirane
+{
+    internal class Rectangle : Shape
+    {
+
+         public double Width{ get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double calculatePerimeter()
+        {
+            return (Width + Height) * 2;
+        }
+
+        public override double calculateArea()
+        {
+            return Width * Height;
+        }
+    }
+}
